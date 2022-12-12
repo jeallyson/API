fa064d329ffcb81cdc9172a4881645821ca4b222
vivian <118554211+vivski@users.noreply.github.com>
2022-12-12 08:40:46 -0300

    modificando todo site

---
+++ main.js
@@ -0,0 +1,32 @@
+async function cotacao()
+{
+    const respostaCotacao = await fetch ('https://api.hgbrasil.com/finance?key=b33d63c0&format=json-cors')
+    const json = await respostaCotacao.json()
+
+    document.getElementById("usd").innerHTML = json.results.currencies.USD.buy;
+
+    document.getElementById("eur").innerHTML = json.results.currencies.EUR.buy; 
+
+    document.getElementById("gbp").innerHTML = json.results.currencies.GBP.buy;
+
+    document.getElementById("ars").innerHTML = json.results.currencies.ARS.buy;
+
+    document.getElementById("cad").innerHTML = json.results.currencies.CAD.buy;
+
+    document.getElementById("aud").innerHTML = json.results.currencies.AUD.buy;
+
+    document.getElementById("jpy").innerHTML = json.results.currencies.JPY.buy;
+
+    document.getElementById("cny").innerHTML = json.results.currencies.CNY.buy;
+
+    document.getElementById("btc").innerHTML = json.results.currencies.BTC.buy;
+
+    document.getElementById("coinbase").innerHTML = json.results.bitcoin.coinbase.last
+
+    document.getElementById("fox-bit").innerHTML = json.results.bitcoin.FoxBit.last
+
+}
+
+
+
+cotacao();
